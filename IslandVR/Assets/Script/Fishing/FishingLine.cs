472f3f019607b56f2f7e9bf0a917a527cdbfe292
Dulfins <vguan005@gmail.com>
2023-05-17 14:13:34 +1200

    Fixed Fishing Rod Rope Bug
    Fixed a bug where fishing rod rope wasn't showing up

--- IslandVR/Assets/Script/Fishing/FishingLine.cs
+++ IslandVR/Assets/Script/Fishing/FishingLine.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class FishingLine : MonoBehaviour
 {
     public LineRenderer line;
-
     public Transform currentPos;
-
     public Transform lurePos;
+    public Transform midPoint;
+    public float vertexCount = 12;
+    public float midPointYPosition = 2;
 
     private void Start()
     {
@@ -18,10 +19,22 @@ public class FishingLine : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        line.SetPosition(0, currentPos.localPosition);
-        line.SetPosition(1, lurePos.localPosition);
-        Debug.Log("CurrentPos:"+ currentPos.position);
-        
+        midPoint.transform.position = new Vector3(
+            (currentPos.transform.position.x + lurePos.transform.position.x),
+            midPointYPosition,
+            (currentPos.transform.position.z + lurePos.transform.position.z) / 2);
+        var PointList = new List<Vector3>();
 
+        for (float ratio = 0;ratio<=1;ratio+=1/vertexCount)
+        {
+            var tangent1 = Vector3.Lerp(currentPos.localPosition, lurePos.localPosition, ratio);
+            var tangent2 = Vector3.Lerp(currentPos.localPosition, lurePos.localPosition, ratio);
+            var curve = Vector3.Lerp(tangent1, tangent2, ratio);
+
+            PointList.Add(curve);
+        }
+
+        line.positionCount = PointList.Count;
+        line.SetPositions(PointList.ToArray());
     }
 }
